9d7b9a91588839504d5269cf7dfcfddc00ec882d
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-01-30 03:00:48 +0500

    Indexed Array , Associative Array , MultiDimensional Array.

--- arrays.php
+++ arrays.php
@@ -2,36 +2,33 @@
 
 // ======  indexed Array   ==========
 
-// $data = array("Mouse", "keyboard", 23,12);
-// echo $data[0];
-// echo $data[1];
-// $newData =  $data[0];
+$data = array("Mouse", "keyboard", 23,12);
+echo $data[0];
+echo $data[1];
+$newData =  $data[0];
 
 
 //   ======  Associative Array ======
 
-// $data = array("name"=>"Ahmed", "email"=>"ahmed@gmail.com","status"=>"Developer");
+$data = array("name"=>"Ahmed", "email"=>"ahmed@gmail.com","status"=>"Developer");
 
-// echo $data["name"];
-// echo $data["email"];
-// echo $data["status"];
+echo $data["name"];
+echo $data["email"];
+echo $data["status"];
 
 
 
 // ====== MultiDimentional Array   =========
 
-// $data = array(
+$data = array(
 
-// "salik" => array("Phone"=>"+923654889", "email"=>"salik@gmail.com","status"=>"BackendDeveloper"),
-// "usman" => array("Phone"=>"+9256454", "email"=>"usman@gmail.com","status"=>"FrontendDeveloper"),
-// );
+"salik" => array("Phone"=>"+923654889", "email"=>"salik@gmail.com","status"=>"BackendDeveloper"),
+"usman" => array("Phone"=>"+9256454", "email"=>"usman@gmail.com","status"=>"FrontendDeveloper"),
+);
 
-// echo $data["salik"]["email"];
-// echo "<br>";
-// echo $data["usman"]["email"]
-
-
-
+echo $data["salik"]["email"];
+echo "<br>";
+echo $data["usman"]["email"]
 
 
 ?>
