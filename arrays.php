da3c1ffd13238153c0a0f7f43648574a908d0d1e
AyeshaSangani <142738168+AyeshaSangani@users.noreply.github.com>
2025-01-30 03:19:34 +0500

    Indexed Array , Associative Array , MultiDimensional Array.

--- arrays.php
+++ arrays.php
@@ -32,3 +32,23 @@ echo $data["usman"]["email"]
 
 
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARRAYS</title>
+</head>
+<body>
+    
+    <h1>Arrays in PHP</h1>
+
+    <h3><b>  <?php echo  $newData ?>  </b></h3>
+
+
+</body>
+</html>
